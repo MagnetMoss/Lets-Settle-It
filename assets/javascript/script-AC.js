43c76b9af9ac97b32315c77d159b7d70fc9b1250
A.C.Kimari <annchristine704@gmail.com>
2021-07-09 10:18:16 -0700

    added var for users in Javascript and lined up the input boxes and random movie selector box

--- assets/javascript/script-AC.js
+++ assets/javascript/script-AC.js
@@ -2,6 +2,11 @@ var input1 = document.getElementById('input-1');
 var input2 = document.getElementById('input-2');
 var input3 = document.getElementById('input-3');
 var input4 = document.getElementById('input-4');
+var user1 = document.getElementById('user1');
+var user2 = document.getElementById('user2');
+var user3 = document.getElementById('user3');
+var user4 = document.getElementById('user4');
+
 var submitBtn = document.getElementById('submitBtn');
 
 submitBtn.addEventListener('click', function() {
@@ -16,4 +21,7 @@ submitBtn.addEventListener('click', function() {
     storedArray.push(newArray);
 
     localStorage.setItem('CustomInput', JSON.stringify(storedArray));
+
+    var userArray = [user1.value, user2.value, user3.value, user4.value];
+    console.log(userArray);
 });

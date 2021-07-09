9f0f4d2790991efd0d239e00bb10397f3c2da559
Jaiar Rose <LFJ143@gmail.com>
2021-07-09 13:03:35 -0700

    refreshed query selectors

--- assets/javascript/script-JR.js
+++ assets/javascript/script-JR.js
@@ -1,12 +1,27 @@
-var numPlayers = document.forms[0].elements[0].value
-var typeCon = document.querySelector("conflict")
-var conflictRes = document.querySelector("conRes")
+var numPlayers = document.querySelector("#players")
+var typeCon = document.querySelector("#conflict")
+var conflictRes = document.querySelector("#conRes")
+var submitBtn = document.querySelector("#submit")
 
 console.log(numPlayers)
 console.log(typeCon)
 console.log(conflictRes)
 
-function locStor () {
-    var store= localStorage.setItem(numPlayers);
-    console.log(store)
-}
+typeCon.style.display= "none"
+conflictRes.style.display= "none"
+
+numPlayers.addEventListener("change", function () {
+    typeCon.style.display= "block"
+})
+
+typeCon.addEventListener("change", function () {
+    conflictRes.style.display= "block"
+})
+
+submitBtn.addEventListener("click" , function (){
+    console.log("click click click");
+})
+
+//if custom direct to wheel
+//if movie direct to sim
+//hide  otherwise
